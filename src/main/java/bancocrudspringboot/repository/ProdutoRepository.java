ef314f1033f9d5e5ea037bb3003861d48571a7f4
adriano.kalbusch <adrianok.work@gmail.com>
2024-05-29 22:15:19 -0300

    ok

--- src/main/java/bancocrudspringboot/repository/ProdutoRepository.java
+++ src/main/java/bancocrudspringboot/repository/ProdutoRepository.java
@@ -1,11 +1,16 @@
 package bancocrudspringboot.repository;
 
+import java.util.List;
+
 import bancocrudspringboot.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 
-
+    @Query(value="select p.* from produto p where descricao ilike concat('%', :descricao, '%')", nativeQuery = true)
+    List<Produto>findProdutoByDescricao(@Param("descricao")String descricao);
 }
